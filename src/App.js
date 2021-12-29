bc22c9eaf00e8e284a1cf5fb6e7ede9ca83332d2
khizar_BM <khizarbm@gmail.com>
2021-12-29 20:40:41 +0500

    Fixed pagination bug for less than 5 movies

--- src/App.js
+++ src/App.js
@@ -27,49 +27,50 @@ const movies = [{
     genre: "Drama",
     rating: 7.4,
     release_year: "2021"
-}, {
-    id: 5,
-    name: "Red Notice",
-    genre: "Thriller",
-    rating: 6,
-    release_year: "2018"
-}, {
-    id: 6,
-    name: "Jungle Cruise",
-    genre: "Adventure",
-    rating: 6.4,
-    release_year: "2020"
-}, {
-    id: 7,
-    name: "Dracula",
-    genre: "Thriller",
-    rating: 7,
-    release_year: "2016"
-}, {
-    id: 8,
-    name: "The Amazing Spider-man",
-    genre: "Sci-Fi",
-    rating: 6.8,
-    release_year: "2012"
-}, {
-    id: 9,
-    name: "IT",
-    genre: "Horror",
-    rating: 7.9,
-    release_year: "2016"
-}, {
-    id: 10,
-    name: "The Dark Knight",
-    genre: "Action",
-    rating: 9,
-    release_year: "2008"
-},{
-    id: 11,
-    name: "The Dark Knight Rises",
-    genre: "Action",
-    rating: 9,
-    release_year: "2008"
-}]
+},
+    {
+        id: 5,
+        name: "Red Notice",
+        genre: "Thriller",
+        rating: 6,
+        release_year: "2018"
+    }, {
+        id: 6,
+        name: "Jungle Cruise",
+        genre: "Adventure",
+        rating: 6.4,
+        release_year: "2020"
+    }, {
+        id: 7,
+        name: "Dracula",
+        genre: "Thriller",
+        rating: 7,
+        release_year: "2016"
+    }, {
+        id: 8,
+        name: "The Amazing Spider-man",
+        genre: "Sci-Fi",
+        rating: 6.8,
+        release_year: "2012"
+    }, {
+        id: 9,
+        name: "IT",
+        genre: "Horror",
+        rating: 7.9,
+        release_year: "2016"
+    }, {
+        id: 10,
+        name: "The Dark Knight",
+        genre: "Action",
+        rating: 9,
+        release_year: "2008"
+    },{
+        id: 11,
+        name: "The Dark Knight Rises",
+        genre: "Action",
+        rating: 9,
+        release_year: "2008"
+    }]
 
 function App() {
     const [pageCount, setPageCount] = useState(0);
@@ -77,12 +78,13 @@ function App() {
     useEffect(() => {
         if (movies.length > 5){
             setMoviesOnPage(movies.slice(0,5))
+            setPageCount(Math.ceil(movies.length/5));
         }
         else {
             setMoviesOnPage(movies)
+            setPageCount(0)
         }
 
-        setPageCount(Math.ceil(movies.length/5));
     }, []);
 
     const changePage = (pageNumber) => {
@@ -94,11 +96,11 @@ function App() {
         <div className='container'>
             <h1 className='title'>Movies</h1>
             <TableComponent movies={moviesOnPage}/>
-            <div className='btn-container'>
+            {pageCount!==0? <div className='btn-container'>
                 {[...Array(pageCount)].map((x, i) =>
                     <PaginationButtonComponent key={i} pageNumber={i+1} onClick={changePage}/>
                 )}
-            </div>
+            </div>:''}
         </div>
     );
 }
