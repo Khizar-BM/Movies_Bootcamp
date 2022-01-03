3f6e67b0015e34c30d544473217e2f0de958343d
khizar_BM <khizarbm@gmail.com>
2022-01-03 23:40:00 +0500

    added basic filtering functionality with css

--- src/components/dashboard/DashboardComponent.js
+++ src/components/dashboard/DashboardComponent.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import movies from "../../MovieList";
+import moviesConstant from "../../MovieList";
 import TableComponent from "../table/TableComponent";
 import PaginationButtonComponent from "../../utils/pagination/PaginationButtonComponent";
 import classes from "./DashboardComponent.module.css";
@@ -8,6 +8,7 @@ import FilterComponent from "../filter/FilterComponent";
 function DashboardComponent() {
     const [pageCount, setPageCount] = useState(0);
     const [moviesOnPage, setMoviesOnPage] = useState([]);
+    const [movies, setMovies] = useState([...moviesConstant]);
     useEffect(() => {
         if (movies.length > 5) {
             setMoviesOnPage(movies.slice(0, 5));
@@ -17,19 +18,30 @@ function DashboardComponent() {
             setPageCount(0);
         }
 
-    }, []);
+    }, [movies]);
 
     const changePage = (pageNumber) => {
         setMoviesOnPage(movies.slice((pageNumber - 1) * 5, pageNumber * 5));
 
     }
+    const onGenreChange = (genre) => {
+        setMovies(movies.filter(movie => movie.genre === genre));
+    }
+    const onRatingChange = (rating) => {
+        setMovies(movies.filter(movie => movie.rating >= rating));
+    }
+    const onYearChange = (year) => {
+        setMovies(movies.filter(movie => movie.release_year === year));
+    }
 
     return (
         <div className={classes.container}>
             <h1 className={classes.title}>Movies</h1>
-            <FilterComponent filterName='Genre' filterList={[...new Set(movies.map(movie => movie.genre))]}/>
-            <FilterComponent filterName='Rating' filterList={[...new Set(movies.map(movie => movie.rating))]}/>
-            <FilterComponent filterName='Year' filterList={[...new Set(movies.map(movie => movie.release_year))]}/>
+            <div className={classes.filtercontainer}>
+                <FilterComponent filterName='Genre' filterList={[...new Set(moviesConstant.map(movie => movie.genre))].sort()} onOptionChange={onGenreChange}/>
+                <FilterComponent filterName='Rating' filterList={[...new Set(moviesConstant.map(movie => movie.rating))].sort()} onOptionChange={onRatingChange}/>
+                <FilterComponent filterName='Year' filterList={[...new Set(moviesConstant.map(movie => movie.release_year))].sort()} onOptionChange={onYearChange}/>
+            </div>
             <TableComponent movies={moviesOnPage}/>
             {pageCount !== 0 ? <div className={classes.btncontainer}>
                 {[...Array(pageCount)].map((x, i) =>
--- src/components/filter/FilterComponent.js
+++ src/components/filter/FilterComponent.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import classes from "./FilterComponent.module.css"
+const FilterComponent = ({filterName, filterList, onOptionChange}) => {
+    const option = useRef();
+    return (<div className={classes.container}>
+        <label htmlFor={filterName}><b>{filterName}:</b></label>
 
-const FilterComponent = ({filterName, filterList}) => {
-    return (<div>
-        <label htmlFor={filterName}>{filterName}:</label>
-
-        <select name={filterName} id={filterName}>
+        <select name={filterName} id={filterName} onChange={()=>onOptionChange(option.current.value)} ref={option}>
             <option value={null}>--Please choose an option--</option>
             {filterList.map((item, i) => <option key={i} value={item}>{item}</option>)}
         </select>
