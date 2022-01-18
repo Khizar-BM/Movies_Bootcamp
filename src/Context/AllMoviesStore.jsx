c03cae427fcf7a55d07621a16d0c98d19df0f12f
khizar_BM <khizarbm@gmail.com>
2022-01-18 15:35:53 +0500

    minor code changes as discussed in standup

--- src/Context/AllMoviesStore.jsx
+++ src/Context/AllMoviesStore.jsx
@@ -1,20 +1,23 @@
 import React, {createContext, useReducer} from 'react';
-import {movieList} from "../ExternalData";
 
 export const AllMoviesContext = createContext({})
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            return [...state, {...action.movie, id: state.length + 1}]
+        case "EDIT":
+            return state.map(movieItem => (movieItem.id === action.movie.id ? action.movie : movieItem))
+        case "LOAD-MOVIES":
+            return [...action.movies]
+        default:
+            return state
+
+    }
+}
+const initialState =[]
 
 const AllMoviesStore = (props) => {
-    const [allMovies, dispatch] = useReducer((state, action) => {
-        switch (action.type) {
-            case "ADD":
-                return [...state, {...action.movie, id: state.length + 1}]
-            case "EDIT":
-                return state.map(movieItem => (movieItem.id === action.movie.id ? action.movie : movieItem))
-            default:
-                return state
-
-        }
-    }, [...movieList]);
+    const [allMovies, dispatch] = useReducer(reducer, initialState);
 
     return (<AllMoviesContext.Provider value={{allMovies, dispatch}}>
         {props.children}
--- src/Pages/AllMovies/AllMoviesPage.jsx
+++ src/Pages/AllMovies/AllMoviesPage.jsx
@@ -1,16 +1,25 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import TableComponent from "../../components/table/TableComponent";
 import PaginationComponent from "../../components/Pagination/PaginationComponent/PaginationComponent";
 import FilteringComponent from "../../components/filter/FilteringComponent/FilteringComponent";
 import classes from "./AllMoviesPage.module.css";
 import {Link} from "react-router-dom";
 import {AllMoviesContext} from "../../Context/AllMoviesStore";
+import {movieList} from "../../ExternalData";
 
 const AllMoviesPage = () => {
-    const {allMovies} = useContext(AllMoviesContext)
+    const {allMovies, dispatch} = useContext(AllMoviesContext)
 
     const [filteredMovies, setFilteredMovies] = useState([...allMovies]);
     const [moviesOnPage, setMoviesOnPage] = useState([]);
+
+    useEffect(() => {
+        if (allMovies.length === 0) {
+            dispatch({type: "LOAD-MOVIES", movies: movieList})
+        }
+
+    }, []);
+
 
     return (
         <div className={classes.container}>
